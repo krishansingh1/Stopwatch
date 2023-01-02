163bca03818a447ebcbb805b8ff4d51a09ef54c8
Krishan Kumar <kkrishan7074@gmail.com>
2023-01-02 16:38:06 +0530

    Commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("StopWatch");
+// console.log("StopWatch");
+const start_btn = document.getElementById('start_btn');
+
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+
+start_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    startTime(minute, second, millisecond)
+});
+
+function startTime(min, sec, m_sec) {
+    let interval = setInterval(() => {
+        m_sec = m_sec + 1;
+        if (m_sec == 230) {
+            m_sec = 0;
+            sec = sec + 1;
+            if (sec == 60) {
+                sec = 0;
+                min = min + 1;
+            }
+        }
+        console.log(`seconds:${sec},minute:${min}`);
+    }, 1);
+}
+
+function changeIcon(anchor) {
+    var icon = anchor.querySelector('i');
+    icon.classList.toggle('fa-play');
+    icon.classList.toggle('fa-pause');
+
+}
