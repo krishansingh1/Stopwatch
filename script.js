131cde36a66243432d9e998755abba296f9bc424
Krishan Kumar <kkrishan7074@gmail.com>
2023-01-02 21:54:37 +0530

    Commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // console.log("StopWatch");
 const start_btn = $('#start_btn');
+const timer = $("#timer");
 
 let minute = 0;
 let second = 0;
 let millisecond = 0;
 let clicked = false;
 let interval;
+let time;
 
 function startTime(min, sec, m_sec) {
     interval = setInterval(() => {
         m_sec = m_sec + 1;
-        if (m_sec == 230) {
+        if (m_sec == 250) {
             m_sec = 0;
             sec = sec + 1;
             if (sec == 60) {
@@ -18,7 +20,9 @@ function startTime(min, sec, m_sec) {
                 min = min + 1;
             }
         }
-        console.log(`seconds:${sec},minute:${min}`);
+        // console.log(`seconds:${sec},minute:${min}`);
+        time = `${min}:${sec}:${m_sec}`
+        timer.html(time);
     }, 1);
 }
 
