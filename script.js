5bfb3ccacf576bbc76a0416b2cceaa3f95b35268
Krishan Kumar <kkrishan7074@gmail.com>
2023-01-02 16:57:01 +0530

    Commit

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 // console.log("StopWatch");
-const start_btn = document.getElementById('start_btn');
+const start_btn = $('#start_btn');
 
 let minute = 0;
 let second = 0;
 let millisecond = 0;
-
-
-start_btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    startTime(minute, second, millisecond)
-});
+let clicked = false;
+let interval;
 
 function startTime(min, sec, m_sec) {
-    let interval = setInterval(() => {
+    interval = setInterval(() => {
         m_sec = m_sec + 1;
         if (m_sec == 230) {
             m_sec = 0;
@@ -30,5 +26,15 @@ function changeIcon(anchor) {
     var icon = anchor.querySelector('i');
     icon.classList.toggle('fa-play');
     icon.classList.toggle('fa-pause');
-
 }
+
+start_btn.click(function () {
+    if (clicked) {
+        $(this).off('click');
+        clearInterval(interval)
+        clicked = false;
+    } else {
+        startTime(minute, second, millisecond);
+        clicked = true;
+    }
+});
